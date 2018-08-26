4f7f4836a296782c694dd0c0d77573c7dcbec17b
Gao Wang <gaow@uchicago.edu>
2018-08-26 18:46:57 -0500

    Add generated HTML files

---
+++ docs/js/docs.js
@@ -0,0 +1,3 @@
+var analysisArray = ['00results']
+var analysisDict = {'knitr::opts-chunk-set(echo-TRUE)-1': '00results'}
+var analysisArrayMap = {'00results': 'knitr::opts_chunk$set(echo = TRUE)'}
---
+++ docs/site_libs/tipuesearch/tipuesearch_content.js
@@ -0,0 +1,8 @@
+var tipuesearch = {"pages": [
+{"mainTitle": "Analysis", "title": "Analysis", "text": "Analysis ", "tags": "", "mainUrl": "http://KaiqianZhang.github.io/dsc-susie/analysis.html", "url": "http://KaiqianZhang.github.io/dsc-susie/analysis.html#Analysis"},
+{"mainTitle": "dsc-susie", "title": "dsc-susie", "text": "dsc-susie ", "tags": "", "mainUrl": "http://KaiqianZhang.github.io/dsc-susie/index.html", "url": "http://KaiqianZhang.github.io/dsc-susie/index.html#dsc-susie"},
+{"mainTitle": "dsc-susie", "title": "The goal:", "text": "The goal ", "tags": "", "mainUrl": "http://KaiqianZhang.github.io/dsc-susie/index.html", "url": "http://KaiqianZhang.github.io/dsc-susie/index.html#The-goal:"},
+{"mainTitle": "dsc-susie", "title": "Data and simulation", "text": "Data and simulation ", "tags": "", "mainUrl": "http://KaiqianZhang.github.io/dsc-susie/index.html", "url": "http://KaiqianZhang.github.io/dsc-susie/index.html#Data-and-simulation"},
+{"mainTitle": "dsc-susie", "title": "Methods", "text": "Methods ", "tags": "", "mainUrl": "http://KaiqianZhang.github.io/dsc-susie/index.html", "url": "http://KaiqianZhang.github.io/dsc-susie/index.html#Methods"},
+{"mainTitle": "dsc-susie", "title": "Run DSC", "text": "Run DSC ", "tags": "", "mainUrl": "http://KaiqianZhang.github.io/dsc-susie/index.html", "url": "http://KaiqianZhang.github.io/dsc-susie/index.html#Run-DSC"}
+]};
